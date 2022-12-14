77a38ea5578e6eb96e8b5ed2af477d1593b6816c
Giacomo Torti <giacomoto@pop-os.localdomain>
2022-12-14 16:06:37 +0100

    coordinate constraints

---
+++ Trait/CoordinateConstraintTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Luckyseven\Bundle\LuckysevenValidationBundle\Trait;
+
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Constraints\LessThanOrEqual;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
+
+trait CoordinateConstraintTrait
+{
+    /**
+     * @return array
+     */
+    public function isTypeLatitude(): array {
+        return [
+            new NotBlank(),
+            new Type('numeric'),
+            new LessThanOrEqual(90),
+            new GreaterThanOrEqual(-90),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function isTypeLongitude(): array {
+        return [
+            new NotBlank(),
+            new Type('numeric'),
+            new LessThanOrEqual(180),
+            new GreaterThanOrEqual(-180),
+        ];
+    }
+}
