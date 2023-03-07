2ce00c5c77222e5bb34aee9164359528ee5abdbe
Giacomo Torti <giacomoto@pop-os.localdomain>
2023-03-07 11:17:04 +0100

    Separated Unique Validation from Email

--- Trait/TUniqueConstraints.php
+++ Trait/TUniqueConstraints.php
@@ -1,54 +1,24 @@
 <?php
 
-namespace Luckyseven\Bundle\LuckysevenValidationBundle\Validator;
+namespace Luckyseven\Bundle\LuckysevenValidationBundle\Trait;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Validator\Constraint;
-use Symfony\Component\Validator\ConstraintValidator;
-use Symfony\Component\Validator\Exception\UnexpectedTypeException;
-use Symfony\Component\Validator\Exception\UnexpectedValueException;
+use Luckyseven\Bundle\LuckysevenValidationBundle\Validator\UniqueConstraint;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
-class UniqueConstraintValidator extends ConstraintValidator
+trait TUniqueConstraints
 {
-    public function __construct(protected EntityManagerInterface $entityManager)
+    /**
+     * @param string|null $entity
+     * @param string $fieldName
+     * @param array $notIn
+     * @return array
+     */
+    protected function isUnique(string $entity, string $fieldName, array $notIn = []): array
     {
-    }
-
-    public function validate($value, Constraint $constraint)
-    {
-        if (!$constraint instanceof UniqueConstraint) {
-            throw new UnexpectedTypeException($constraint, UniqueConstraint::class);
-        }
-
-        // custom constraints should ignore null and empty values to allow
-        // other constraints (NotBlank, NotNull, etc.) to take care of that
-        if (null === $value || '' === $value) {
-            return;
-        }
-
-        if (!is_string($value)) {
-            // throw this exception if your validator cannot handle the passed type so that it can be marked as invalid
-            throw new UnexpectedValueException($value, 'string');
-
-            // separate multiple types using pipes
-            // throw new UnexpectedValueException($value, 'string|int');
-        }
-
-        // access your configuration options like this:
-        // if ('strict' === $constraint->mode) {
-        //     // ...
-        // }
-
-        if ($constraint->entity) {
-            $data = $this->entityManager->getRepository($constraint->entity)->findBy([$constraint->fieldName => $value]);
-            foreach ($data as $datum) {
-                if (!in_array($datum->getId(), $constraint->notIn)) {
-                    $this->context->buildViolation($constraint->message)
-                        ->setParameter('{{ string }}', $value)
-                        ->addViolation();
-                    break;
-                }
-            }
-        }
+        return [
+            new UniqueConstraint($entity, $fieldName, $notIn)
+        ];
     }
 }
